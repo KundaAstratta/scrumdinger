92be80febc2f011fb4b6cf13222b72f2a7cbe71d
KundaAstratta <krystof.humbert@gmail.com>
2021-04-30 15:07:51 +0200

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Christophe Humbert on 30/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
