65f20c716384da2bbd488505be4959f3c36c4094
KundaAstratta <krystof.humbert@gmail.com>
2021-05-01 23:20:37 +0200

    first commit

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Christophe Humbert on 30/04/2021.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack (alignment: .leading){
+                    Text("Seconds Elapsed").font(.caption)
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack (alignment: .trailing) {
+                    Text("Seconds Remaining").font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+            Circle().strokeBorder(lineWidth: 24, antialiased: true)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
