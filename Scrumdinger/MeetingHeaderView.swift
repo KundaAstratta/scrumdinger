f0729a0f026c4c2d62d0b7400c0010bb6d2cb37c
KundaAstratta <krystof.humbert@gmail.com>
2021-06-02 21:30:40 +0200

    add design elements to the meetinng header

--- Scrumdinger/MeetingHeaderView.swift
+++ Scrumdinger/MeetingHeaderView.swift
@@ -21,9 +21,11 @@ struct MeetingHeaderView: View {
     private var minutesRemainingMetric: String {
         minutesRemaining == 1 ? "minute" : "minutes"
     }
+    let scrumColor: Color
     var body: some View {
         VStack {
         ProgressView(value: progress)
+            .progressViewStyle(ScrumProgressViewStyle(scrumColor: scrumColor))
         HStack {
             VStack (alignment: .leading){
                 Text("Seconds Elapsed").font(.caption)
@@ -32,19 +34,24 @@ struct MeetingHeaderView: View {
             Spacer()
             VStack (alignment: .trailing) {
                 Text("Seconds Remaining").font(.caption)
-                Label("\(secondsRemaining)", systemImage: "hourglass.bottomhalf.fill")
+                
+                HStack {
+                    Text("\(secondsRemaining)")
+                    Image(systemName: "hourglass.tophalf.fill")
+                }
             }
           }
         }
         .accessibilityElement(children: .ignore)
         .accessibilityLabel(Text("Time remaining"))
         .accessibilityValue(Text("\(minutesRemaining) \(minutesRemainingMetric)"))
+        .padding([.top, .horizontal])
     }
 }
 
 struct MeetingHeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        MeetingHeaderView(secondsElapsed: 60, secondsRemaining: 180)
+        MeetingHeaderView(secondsElapsed: 60, secondsRemaining: 180, scrumColor: DailyScrum.data[0].color)
             .previewLayout(.sizeThatFits)
     }
 }
