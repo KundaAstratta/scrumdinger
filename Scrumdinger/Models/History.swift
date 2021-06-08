a9222fac207c22e2b4cf8714431187b0c003a4c1
KundaAstratta <krystof.humbert@gmail.com>
2021-06-08 23:47:26 +0200

    Scrum History

---
+++ Scrumdinger/Models/History.swift
@@ -0,0 +1,27 @@
+//
+//  History.swift
+//  Scrumdinger
+//
+//  Created by Christophe Humbert on 08/06/2021.
+//
+
+/*
+See LICENSE folder for this sample’s licensing information.
+*/
+
+import Foundation
+
+struct History: Identifiable {
+    let id: UUID
+    let date: Date
+    var attendees: [String]
+    var lengthInMinutes: Int
+
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [String], lengthInMinutes: Int) {
+        self.id = id
+        self.date = date
+        self.attendees = attendees
+        self.lengthInMinutes = lengthInMinutes
+    }
+}
+
