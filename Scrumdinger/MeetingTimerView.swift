8632b6dd50d727484f5d0f847638f550ba236861
KundaAstratta <krystof.humbert@gmail.com>
2021-11-02 11:25:08 +0100

    Drawing the timer view

---
+++ Scrumdinger/MeetingTimerView.swift
@@ -0,0 +1,34 @@
+//
+//  MeetingTimerView.swift
+//  Scrumdinger
+//
+//  Created by C HUMBERT on 02/11/2021.
+//
+
+import SwiftUI
+
+struct MeetingTimerView: View {
+    let speakers: [ScrumTimer.Speaker]
+    var scrumColor: Color
+    private var currentSpeaker: String { speakers.first(where: { !$0.isCompleted })?.name ?? "Someone" }
+    var body: some View {
+        ZStack{
+            Circle().strokeBorder(lineWidth: 24, antialiased: true)
+            VStack{
+                Text(currentSpeaker).font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                Text("is speaking")
+            }
+            .accessibilityElement(children: .combine)
+            .foregroundColor(scrumColor.accessibleFontColor)
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct MeetingTimerView_Previews: PreviewProvider {
+    static var speakers = [ScrumTimer.Speaker(name: "Kim", isCompleted: true), ScrumTimer.Speaker(name: "Bill", isCompleted: false)]
+
+    static var previews: some View {
+        MeetingTimerView(speakers: speakers, scrumColor: Color("Design"))
+    }
+}
